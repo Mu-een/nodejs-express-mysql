c93d5be5cb609ff699f3a04807f94cdbac00c97a
Mu-een <mueenslam@gmail.com>
2023-02-14 11:52:43 +0200

    1st commit

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "bmqamvckv98jtjfz7vtr-mysql.services.clever-cloud.com",
+    USER: "uniju2lphcim3axv",
+    PASSWORD: "GG11OYSZXuHp02VAIYBj",
+    DB: "bmqamvckv98jtjfz7vtr"
+  };
